15c06da4a797451b2132a17820f288d13d966b2f
Aybareon <joaosousa200792@gmail.com>
2016-05-04 16:29:19 +0100

    voter quase la

--- webapp/voter.py
+++ webapp/voter.py
@@ -1,38 +1,84 @@
 from suds.client import Client
 from time import *
 import logging
+import threading
 
-
-
-
+#numero de segundos do timeout
+timeout = 4
+#numero de web services a usar para fazer a request
+useWebservicesNumber = 3
 
 class Voter:
-	wsdlFiles = [
-		"http://insulincalculator-aybareon.rhcloud.com/InsulinCalculatorTomcat/InsulinCalculator?wsdl"
-		]
 
-	sensitivity = 1
 
+	def __init__(self):
+		self.wsdlFiles = [
+			"http://qcsa1-beardsdei.rhcloud.com/qcsa1/InsulinDoseCalculator?wsdl",
+			"http://qcsproject1-qcsproject.rhcloud.com/InsulinDoseCalculator?wsdl",
+			"http://insulincalculator-aybareon.rhcloud.com/InsulinCalculatorTomcat/InsulinCalculator?wsdl",
+			"http://webservice-sqdcourse.rhcloud.com/InsulinDoseCalculator?wsdl"
+			]
+
+		self.sensitivity = 1
+		self.wsClients = []
+		self.wsResults = []	
+		self.voter_start_time = time()
+		self.time_flag=False
+
+	def wsThread(self, thread_id, wsdl_name, method, args):
+		cur = Client(wsdl_name)
+		cur.set_options(timeout=4)
+		self.wsClients.append(cur)
+		start_time = time()
+
+		if method==1:
+			if thread_id == 1:
+				sleep(10)
+				res = [self.wsdlFiles[thread_id],20]
+			else:
+				res = [self.wsdlFiles[thread_id],cur.service.backgroundInsulinDose(args)]
+		elif method==2:
+			res = [self.wsdlFiles[thread_id],cur.service.mealtimeInsulinDose(args)]
+		elif method==3:
+			res = [self.wsdlFiles[thread_id],cur.service.personalSensitivity(args)]
+
+		if self.time_flag==False:
+			self.wsResults.append(res)
+		print str(thread_id)+"\ntime wasted on this webservice: " + str(time()-start_time)
 
 	def calcResult(self, method, args):
-		wsClients = []
-		wsResults = []
+
 		realResults = []
+		ws_threads = []
 
+		#start_time = time()
+
+		thread_id = 0
 		for item in self.wsdlFiles:
-			cur = Client(item)
-			wsClients.append(cur)
-			wsResults.append(cur.service.backgroundInsulinDose(args))
+			cur = threading.Thread(target = self.wsThread, args=(thread_id, item, method, args,))
+			ws_threads.append(cur)
+			cur.start()
+			thread_id = thread_id+1
 
-		for item in wsResults:
+		#verificar se ja passou timeout ou se ja temos todos os resultados
+		while time()-self.voter_start_time  < 4:
+			if len(self.wsResults) >= useWebservicesNumber or len(self.wsResults) >= len(self.wsdlFiles):
+				break;
+
+		self.time_flag=True
+		
+
+		print "\ntime wasted!: " + str(time()-self.voter_start_time)
+
+		for item in self.wsResults:
 			exists = False
 			for result in range (len(realResults)):
-				if item <= realResults[result][0]+sensitivity and item >= realResults[result][0]-sensitivity:
+				if item[1] <= realResults[result][0]+self.sensitivity and item[1] >= realResults[result][0]-self.sensitivity:
 					realResults[result][1] = realResults[result][1]+1
-				if item == realResults[result][0]:
+				if item[1] == realResults[result][0]:
 					exists = True
 			if exists == False:
-				realResults.append([item,1])
+				realResults.append([item[1],1])
 
 		bestVal = -1
 		bestOccur = 0
@@ -44,6 +90,11 @@ class Voter:
 				valid = True
 			elif realResults[item][0] == bestVal:
 				valid = False
+
+
+		print("\ndetailed results:")
+		for item in self.wsResults:
+			print "\nwebservice: "+ item[0] + "\tresult: " + str(item[1])
 
 		if valid:
 			return (bestVal,bestOccur)
